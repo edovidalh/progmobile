168f848a7db2e322d76bade01c1e2f1fac3dc631
edovidalh <evidalhevia@gmail.com>
2021-10-01 07:51:08 -0300

    inicio sesion casi completo

--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  data: any = {
+    Usuario: "",
+    Contraseña: ""
   }
+  field:string="";
 
+constructor(public toastController: ToastController) {}
+ngOnInit() {}
+
+ingresar(){
+  if(this.validateModel(this.data)){
+    this.presentToast("Bienvenido");
+  }
+  else{
+    this.presentToast("Falta: "+this.field);
+  }
+}
+
+validateModel(model:any){
+  
+  for (var [key, value] of Object.entries(model)) {
+    
+    if (value=="") {
+     
+      this.field=key;
+
+      return false;
+    }
+  }
+  return true;
+}
+
+async presentToast(message:string, duration?:number){
+  const toast = await this.toastController.create(
+    {
+      message:message,
+      duration:duration?duration:2000
+    }
+  );
+  toast.present();
+}
 }
